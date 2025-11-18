2bfefcb64383b471c4c65514845584069ac068d2
Jugurtha Bellagh <jugubell@proton.me>
2025-11-18 23:12:31 +0100

    Adding LICENSE file and license notice

--- web/src/lib/components/Header.svelte
+++ web/src/lib/components/Header.svelte
@@ -1,3 +1,26 @@
+<!--
+  - File: Header.svelte
+  - Project: bproc-web
+  - Last modified: 2025-11-18 23:11
+  -
+  - This file: Header.svelte is part of BProC-WEB project.
+  -
+  - BProC-WEB is free software: you can redistribute it and/or modify it
+  - under the terms of the GNU General Public License as published
+  - by the Free Software Foundation, either version 2 of the License,
+  - or (at your option) any later version.
+  -
+  - BProC-WEB is distributed in the hope that it will be useful,
+  - but WITHOUT ANY WARRANTY; without even the implied warranty
+  - of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+  - See the GNU General Public License for more details.
+  -
+  - You should have received a copy of the GNU General Public License
+  - along with BProC-WEB. If not, see <https://www.gnu.org/licenses/>.
+  -
+  - Copyright (C) 2025 Jugurtha Bellagh
+  -->
+
 <script>
 	import bproc_logo from '$lib/assets/bproc_logo.svg';
 </script>
--- web/svelte.config.js
+++ web/svelte.config.js
@@ -1,3 +1,26 @@
+/*
+ * File: svelte.config.js
+ * Project: bproc-web
+ * Last modified: 2025-11-18 23:11
+ *
+ * This file: svelte.config.js is part of BProC-WEB project.
+ *
+ * BProC-WEB is free software: you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License as published
+ * by the Free Software Foundation, either version 2 of the License,
+ * or (at your option) any later version.
+ *
+ * BProC-WEB is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty
+ * of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with BProC-WEB. If not, see <https://www.gnu.org/licenses/>.
+ *
+ * Copyright (C) 2025 Jugurtha Bellagh
+ */
+
 import adapter from '@sveltejs/adapter-auto';
 
 /** @type {import('@sveltejs/kit').Config} */
